957d7f0dc64873a77dfceab9a7cf73f12fa56546
vitor-mda <vitor-mda@hotmail.com>
2022-05-27 23:16:22 -0300

    ⚡️ perf: add `id` to query for smarter caching
    Add company `id` to queried fields on `getJobs` query for smarter
    caching

--- job-board/client/src/graphql/queries.js
+++ job-board/client/src/graphql/queries.js
@@ -14,6 +14,7 @@ export async function getJobs() {
                 id
                 title
                 company {
+                    id
                     name
                 }
             }
